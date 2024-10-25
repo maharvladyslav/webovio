c82008fc0b8d6289ac63a8ac4b3ffa7997d75217
danylyoenko <lockmaster19@gmail.com>
2024-10-25 21:28:09 +0300

    pull files out of main branch

---
+++ src/js/menu.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const menu = document.querySelector('.nav__menu');
+const btn = document.querySelector('.burger');
+
+btn.addEventListener('click', function () {
+  this.classList.toggle('active');
+  menu.classList.toggle('open');
+});
