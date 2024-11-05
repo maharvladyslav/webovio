a99f96cfa0168180f74985833fbfa3fed72c5049
danylyoenko <lockmaster19@gmail.com>
2024-11-05 20:19:45 +0200

    fixed

--- src/js/menu.js
+++ src/js/menu.js
@@ -6,6 +6,7 @@ const btn = document.querySelector('.burger');
 btn.addEventListener('click', function () {
   this.classList.toggle('active');
   menu.classList.toggle('open');
+  document.querySelector('body').classList.toggle('no-scroll');
   document.querySelector('.nav__menu').classList.toggle('active');
   document.querySelector('.burger-container').classList.toggle('active');
 });
